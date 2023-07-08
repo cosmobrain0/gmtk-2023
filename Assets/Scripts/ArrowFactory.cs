a6a1cb22efbc32ba834fb9cc7fec83294562a5f3
icoez <78701335+icoez@users.noreply.github.com>
2023-07-08 15:09:24 +0100

    Added arrow bouncing, hitting the archer detection and out of bounds destruction of arrows

--- Assets/Scripts/ArrowFactory.cs
+++ Assets/Scripts/ArrowFactory.cs
@@ -6,24 +6,28 @@ public class ArrowFactory : MonoBehaviour
 {
     [SerializeField] GameObject target;
     [SerializeField] GameObject arrowPrefab;
+    [SerializeField] float fireCooldown;
     [SerializeField] float arrowSpeed;
+    [SerializeField] int arrowCount;
     void Start()
     {
         StartCoroutine(fireArrows());
     }
     private IEnumerator fireArrows()
     {
-        while (true){
+        for (int i = 0; i < arrowCount; i++)
+        {
             createArrow();
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(fireCooldown);
         }
-        
+        Debug.Log("Ran out of arrows!");
     }
     private void createArrow()
     {
         Vector3 offset = target.transform.position - transform.position;
         float angle = Mathf.Atan2(offset.y, offset.x) - Mathf.PI/2;
         GameObject newArrow = Instantiate(arrowPrefab, transform.position, Quaternion.Euler(0,0, angle * (180/Mathf.PI)));
+        newArrow.transform.parent = transform;
         ArrowScript newArrowScript = newArrow.GetComponent<ArrowScript>();
         newArrowScript.arrowSpeed = arrowSpeed;
     }
--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -7,14 +7,42 @@ public class ArrowScript : MonoBehaviour
     public GameObject target;
     public float arrowSpeed;
     private Vector3 direction;
+    private Rigidbody2D rb;
+    private int archerTriggerCounter = 0;
     private void Awake()
     {
-        target = GameObject.Find("Target");
-        direction = target.transform.position - transform.position;
-        direction = direction.normalized;
+        target = FindObjectOfType<TargetControls>().gameObject;
+        direction = (target.transform.position - transform.position).normalized;
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(direction * arrowSpeed);
     }
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        transform.position += direction * arrowSpeed;
+        CheckIfInScreen();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) - Mathf.PI/2;
+        transform.rotation = Quaternion.Euler(0f,0f, angle * (180/Mathf.PI));
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        archerTriggerCounter++;
+        //Because trigger occurs with archer when arrow spawns, so we want the second instance of trigger
+        if (archerTriggerCounter == 2)
+        {
+            Debug.Log("Hit archer!");
+            //TODO: Switch level/reduce archer HP
+        }
+        
+    }
+    private void CheckIfInScreen()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0f));
+        if (Mathf.Abs(transform.position.x) > bounds.x || Mathf.Abs(transform.position.y) > bounds.y)
+        {
+            Destroy(gameObject);
+        }
     }
 }
